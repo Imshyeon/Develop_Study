1cfb6ebca20bb61fdcfeec1b3c3cc5ac9f4623e1
Imshyeon <kzoen0040@naver.com>
2023-11-26 18:04:01 +0900

    11/26 js

--- Javascript/Complete-Udemy/01_Basic/assets/scripts/app.js
+++ Javascript/Complete-Udemy/01_Basic/assets/scripts/app.js
@@ -1,54 +1,56 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-// currentResult = currentResult + userInput.value;
-
-let calculationDescription = `${defaultResult}`;
-
-// 1. userInput에 숫자를 입력하면 해당 숫자가 currentResultOutput에 반영
-// 2. 만약 add, sub, multi, div button을 누르면 currentCalculationOutput이 안보이고 currentResult에 저장 
-//      -> 다음 연산을 위해서 save
-// 3. button을 누르고 그 다음 userInput에 숫자를 입력하면 currentResult에 있던 숫자와 연산
-//      -> currentResultOutput에 출력. 
-let operator;
-
-const add = (value) =>{
-    currentResult += parseInt(value);
-    return currentResult
-}
-const sub = (value) =>{
-    currentResult -= parseInt(value);
-    return currentResult
-}
-const mul = (value) =>{
-    currentResult *= parseInt(value);
-    return currentResult
-}
-const div = (value) =>{
-    currentResult /= parseInt(value);
-    return currentResult;
+function add() {
+  currentResult = currentResult + parseInt(userInput.value); // +userInput.value == parseInt(userInput.value)
+  outputResult(currentResult, '');
 }
 
-addBtn.addEventListener('click', () => {
-    currentResult = add(userInput.value);  
-    calculationDescription += ` + ${userInput.value}`
-    outputResult(currentResult, calculationDescription);
-})
+// add() 이렇게 하면, 자바스크립트는 해당 문장이 나올 때마다 함수를 실행할 것. 
+// 하지만 우리는 클릭을 했을 때 실행이 되길 원하니까~add 함수를 직접 호출하는 대신 자바스크립트에 함수 이름을 제공한다~이 말씀이야
+addBtn.addEventListener("click", add);
 
-subtractBtn.addEventListener('click', () => {
-    currentResult = sub(userInput.value);  
-    calculationDescription += ` - ${userInput.value}`
-    outputResult(currentResult, calculationDescription);
-})
 
-multiplyBtn.addEventListener('click', () => {
-    currentResult = mul(userInput.value);  
-    calculationDescription += ` * ${userInput.value}`
-    outputResult(currentResult, calculationDescription);
-})
+// =============================== 내 첫 코드 ================================
+// let operator;
 
-divideBtn.addEventListener('click', () => {
-    currentResult = div(userInput.value);  
-    calculationDescription += ` / ${userInput.value}`
-    outputResult(currentResult, calculationDescription);
-})
+// const add = (value) =>{
+//     currentResult += parseInt(value);
+//     return currentResult
+// }
+// const sub = (value) =>{
+//     currentResult -= parseInt(value);
+//     return currentResult
+// }
+// const mul = (value) =>{
+//     currentResult *= parseInt(value);
+//     return currentResult
+// }
+// const div = (value) =>{
+//     currentResult /= parseInt(value);
+//     return currentResult;
+// }
+
+// addBtn.addEventListener('click', () => {
+//     currentResult = add(userInput.value);
+//     calculationDescription += ` + ${userInput.value}`
+//     outputResult(currentResult, calculationDescription);
+// })
+
+// subtractBtn.addEventListener('click', () => {
+//     currentResult = sub(userInput.value);
+//     calculationDescription += ` - ${userInput.value}`
+//     outputResult(currentResult, calculationDescription);
+// })
+
+// multiplyBtn.addEventListener('click', () => {
+//     currentResult = mul(userInput.value);
+//     calculationDescription += ` * ${userInput.value}`
+//     outputResult(currentResult, calculationDescription);
+// })
+
+// divideBtn.addEventListener('click', () => {
+//     currentResult = div(userInput.value);
+//     calculationDescription += ` / ${userInput.value}`
+//     outputResult(currentResult, calculationDescription);
+// })
