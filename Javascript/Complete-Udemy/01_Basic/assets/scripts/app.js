ed039bc26d64402dbc7d7f4fcd2fe1514344a9b6
Imshyeon <kzoen0040@naver.com>
2023-11-26 18:24:34 +0900

    계산기 로직 완성 - 코드 리팩터링 적용

--- Javascript/Complete-Udemy/01_Basic/assets/scripts/app.js
+++ Javascript/Complete-Udemy/01_Basic/assets/scripts/app.js
@@ -1,56 +1,46 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  currentResult = currentResult + parseInt(userInput.value); // +userInput.value == parseInt(userInput.value)
-  outputResult(currentResult, '');
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-// add() 이렇게 하면, 자바스크립트는 해당 문장이 나올 때마다 함수를 실행할 것. 
+function createAndWriteOutput(operator, resultBeforeCalc, numberForCalc) {
+  const calculationDescription = `${resultBeforeCalc} ${operator} ${numberForCalc}`;
+  outputResult(currentResult, calculationDescription);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+}
+
+// add() 이렇게 하면, 자바스크립트는 해당 문장이 나올 때마다 함수를 실행할 것.
 // 하지만 우리는 클릭을 했을 때 실행이 되길 원하니까~add 함수를 직접 호출하는 대신 자바스크립트에 함수 이름을 제공한다~이 말씀이야
 addBtn.addEventListener("click", add);
-
-
-// =============================== 내 첫 코드 ================================
-// let operator;
-
-// const add = (value) =>{
-//     currentResult += parseInt(value);
-//     return currentResult
-// }
-// const sub = (value) =>{
-//     currentResult -= parseInt(value);
-//     return currentResult
-// }
-// const mul = (value) =>{
-//     currentResult *= parseInt(value);
-//     return currentResult
-// }
-// const div = (value) =>{
-//     currentResult /= parseInt(value);
-//     return currentResult;
-// }
-
-// addBtn.addEventListener('click', () => {
-//     currentResult = add(userInput.value);
-//     calculationDescription += ` + ${userInput.value}`
-//     outputResult(currentResult, calculationDescription);
-// })
-
-// subtractBtn.addEventListener('click', () => {
-//     currentResult = sub(userInput.value);
-//     calculationDescription += ` - ${userInput.value}`
-//     outputResult(currentResult, calculationDescription);
-// })
-
-// multiplyBtn.addEventListener('click', () => {
-//     currentResult = mul(userInput.value);
-//     calculationDescription += ` * ${userInput.value}`
-//     outputResult(currentResult, calculationDescription);
-// })
-
-// divideBtn.addEventListener('click', () => {
-//     currentResult = div(userInput.value);
-//     calculationDescription += ` / ${userInput.value}`
-//     outputResult(currentResult, calculationDescription);
-// })
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
