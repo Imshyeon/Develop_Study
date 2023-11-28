54d7ba2e18f5b4863e7de0b1f96609f0d1a4bbae
Imshyeon <kzoen0040@naver.com>
2023-11-28 13:49:11 +0900

    10/28 Array와 Object

--- Javascript/Complete-Udemy/01_Basic/README.md
+++ Javascript/Complete-Udemy/01_Basic/README.md
@@ -1,3 +1,9 @@
 # Basic_Javascript
 1. 기본적인 계산기 만들기 : app.js에 계산기 로직을 작성할 예정
    1. Variable & Constants(변수와 상수) &rarr; let(..can change), const(..must not change)
+   2. Data Types
+      1. Numbers &rarr; 2, -3, 93.0718
+      2. Strings &rarr; 'TM', "TAEMIN", `LEE TAEMIN`
+      3. Booleans &rarr; true, false
+      4. Objects &rarr; {name: 'TAEMIN', age: 31}
+      5. Arrays &rarr; [93, 07, 18]
--- Javascript/Complete-Udemy/01_Basic/assets/scripts/app.js
+++ Javascript/Complete-Udemy/01_Basic/assets/scripts/app.js
@@ -1,40 +1,52 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
+// Gets input from input field
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, numberForCalc) {
   const calculationDescription = `${resultBeforeCalc} ${operator} ${numberForCalc}`;
-  outputResult(currentResult, calculationDescription);
+  outputResult(currentResult, calculationDescription); // from vendor.js
 }
 
 function add() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
+  const logEntry = {
+    operation: 'ADD',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntry.operation)
+  console.log(logEntries);
 }
 
 function subtract() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber;
   createAndWriteOutput("-", initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   createAndWriteOutput("*", initialResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   createAndWriteOutput("/", initialResult, enteredNumber);
 }
 
