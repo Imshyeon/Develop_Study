54f1b41dab8500721adbabd558317c59722e9ce1
Imshyeon <kzoen0040@naver.com>
2023-12-15 18:08:36 +0900

    demo project -2 | 영화 생성 및 입력 검증

--- Javascript/Complete-Udemy/06_DOM/demoProject/assets/scripts/app.js
+++ Javascript/Complete-Udemy/06_DOM/demoProject/assets/scripts/app.js
@@ -1,8 +1,10 @@
 const addMovieModalEl = document.querySelector("#add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const backdropEl = document.querySelector("#backdrop");
-const cancelAddMovieButton =
-  addMovieModalEl.querySelector(".modal__actions").children[0];
+const cancelAddMovieButton = addMovieModalEl.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModalEl.querySelectorAll("input");
+const movies = [];
 
 const toggleBackdrop = () => {
   backdropEl.classList.toggle("visible");
@@ -13,8 +15,42 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const clearMovieInput = () => {
+    for (const userInput of userInputs) {
+        userInput.value = '';
+    };
+};
+
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    parseInt(ratingValue) < 1 ||
+    parseInt(ratingValue) > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+    
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+    
+    movies.push(newMovie);
+    console.log(movies);
     toggleMovieModal();
+    clearMovieInput();
 };
 
 const backdropClickHandler = () => {
@@ -23,4 +59,5 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
 backdropEl.addEventListener("click", backdropClickHandler);
-cancelAddMovieButton.addEventListener("click", cancelAddMovie);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
