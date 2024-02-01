1b27ffa49f9ba355fd1ac0dbe398e78a3cc47e5f
Imshyeon <kzoen0040@naver.com>
2024-02-01 14:24:41 +0900

    📝 algorithm

---
+++ Algorithm/Programmers/Lv0/Lv0.py
@@ -0,0 +1,15 @@
+# 1번 문제
+string_msg = 'Spring is beginning'
+int_val = 3
+string_val = "3"
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
+
+# 2번 문제. 피타고라스의 정리
+a = int(input())
+c = int(input())
+
+b_square = c*c-a*a # pow(c,2) - pow(a,2) 혹은 c**2 - a**2
+print(b_square)
