d8627bbb724f4da0f85e8cdfbb70d38d3dfc7daf
Imshyeon <kzoen0040@naver.com>
2024-02-03 16:04:41 +0900

    💄 messageModal - 로딩, 오류를 위한 메시지 모달 추가

--- React/Complete-React/18_React-Practice-FoodOrderApp/src/components/Meals.jsx
+++ React/Complete-React/18_React-Practice-FoodOrderApp/src/components/Meals.jsx
@@ -1,23 +1,57 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import MealItem from "./MealItem";
+import MessageModal from "./MessageModal";
 
 export default function Meals() {
   const [mealDatas, setMealDatas] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
+  const dialog = useRef();
 
   useEffect(() => {
+    setIsFetching(true);
     async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      const resData = await response.json();
-      setMealDatas(resData);
+      try {
+        const response = await fetch("http://localhost:3000/meals");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("상품을 불러오는데 실패했습니다.");
+        }
+        setMealDatas(resData);
+      } catch (error) {
+        setError({
+          message: error.message || "상품을 불러오는데 실패했습니다.",
+        });
+      }
+
+      setIsFetching(false);
     }
 
     fetchMeals();
   }, []);
 
+  if (error) {
+    return (
+      <MessageModal
+        ref={dialog}
+        title="오류가 발생했습니다🥲"
+        message={error.message}
+      />
+    );
+  }
+
   return (
     <div id="meals">
+      {isFetching && (
+        <MessageModal
+          ref={dialog}
+          title="상품을 불러오는 중입니다."
+          message="상품을 불러오는 중이니 조금만 기다려 주십시오."
+        />
+      )}
       {mealDatas.map((mealItem) => {
-        // console.log(mealItem);
         return (
           <MealItem
             key={mealItem.id}
@@ -28,6 +62,7 @@ export default function Meals() {
           />
         );
       })}
+      ;
     </div>
   );
 }
