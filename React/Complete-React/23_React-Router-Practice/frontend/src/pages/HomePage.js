96c008ef93d26e657ab1367d673da04927b6ea5f
Imshyeon <kzoen0040@naver.com>
2024-02-08 15:42:07 +0900

    📝 마무리

--- React/Complete-React/23_React-Router-Practice/frontend/src/pages/HomePage.js
+++ React/Complete-React/23_React-Router-Practice/frontend/src/pages/HomePage.js
@@ -1,8 +1,10 @@
+import PageContent from "../components/PageContent";
+
 function HomePage() {
   return (
-    <>
-      <h1>HomePage</h1>
-    </>
+    <PageContent title="Welcome">
+      <p>리액트 라우터 프로젝트입니다!</p>
+    </PageContent>
   );
 }
 
