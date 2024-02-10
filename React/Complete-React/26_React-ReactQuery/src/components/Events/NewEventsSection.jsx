03d660f514a55f3396c84506ef74a879fcfb1067
Imshyeon <kzoen0040@naver.com>
2024-02-10 16:22:57 +0900

    📝 쿼리 동작 이해 및 구성

--- React/Complete-React/26_React-ReactQuery/src/components/Events/NewEventsSection.jsx
+++ React/Complete-React/26_React-ReactQuery/src/components/Events/NewEventsSection.jsx
@@ -9,7 +9,9 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events"], // 모든 쿼리(전송하는 모든 GET HTTP 요청)에는 쿼리 키가 있다.
     queryFn: fetchEvents, // 해당 함수를 이용해 실제 요청을 전송할 때 실행할 코드를 정의.
-  }); // 자체적으로 http 요청을 전송하고 해당 섹션에 필요한 이벤트 데이터를 가져오고 로딩 상태에 대한 정보를 제공한다.
+    staleTime: 5000,
+    // gcTime: 30000,
+  });
 
   let content;
 
