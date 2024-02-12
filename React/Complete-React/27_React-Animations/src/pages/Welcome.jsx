2c9033ccca3a0a0dd6acb23a7224369441db86a4
Imshyeon <kzoen0040@naver.com>
2024-02-12 16:54:37 +0900

    💫 스크롤 기반 애니메이션

--- React/Complete-React/27_React-Animations/src/pages/Welcome.jsx
+++ React/Complete-React/27_React-Animations/src/pages/Welcome.jsx
@@ -1,24 +1,52 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-import cityImg from '../assets/city.jpg';
-import heroImg from '../assets/hero.png';
+import cityImg from "../assets/city.jpg";
+import heroImg from "../assets/hero.png";
 
 export default function WelcomePage() {
+  const { scrollY } = useScroll();
+  // scrollY : 사용자가 얼마나 스크롤을 내렸는지 픽셀로 알려줌 - scrollX도 있다.
+  // scrollYProgress : 상대적인 스크롤링 값을 제공. 0~1까지 있고 1은 사용자가 완전히 아래까지 스크롤을 내렸음을 0은 내리지 않았음을 나타냄.
+
+  const yCity = useTransform(scrollY, [0, 200], [0, -100]);
+  const opacityCity = useTransform(
+    scrollY,
+    [0, 200, 300, 500],
+    [1, 0.5, 0.5, 0]
+  );
+  // 변환되어야하는 값을 받음. 중단점으로 구성된 배열, 중단점들과 그 중단점들 사이에 있는 값들이 변환되어야하는 값의 배열
+  // 아예 스크롤을 내리지 않으면 opacity : 1 -> 200픽셀 내리면 0.5로 opacity 설정 -> 500픽셀까지 내리면 opacity는 0으로 설정.
+  // 위처럼 값이 변할 때, 컴포넌트는 재실행되지 않는다.
+  const yHero = useTransform(scrollY, [0, 200], [0, -150]);
+  const opacityHero = useTransform(scrollY, [0, 300, 500], [1, 1, 0]);
+  const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 50, 50, 300]);
+  const scaleText = useTransform(scrollY, [0, 300], [1, 1.5]);
+
   return (
     <>
       <header id="welcome-header">
-        <div id="welcome-header-content">
+        <motion.div
+          id="welcome-header-content"
+          style={{ scale: scaleText, y: yText }}
+        >
           <h1>Ready for a challenge?</h1>
           <Link id="cta-link" to="/challenges">
             Get Started
           </Link>
-        </div>
-        <img
+        </motion.div>
+        <motion.img
+          style={{ opacity: opacityCity, y: yCity }} // motion에서 좀 더 향상된 속성. 프레이머 모션이 값의 변화를 감지하여 애니메이션 적용.
           src={cityImg}
           alt="A city skyline touched by sunlight"
           id="city-image"
         />
-        <img src={heroImg} alt="A superhero wearing a cape" id="hero-image" />
+        <motion.img
+          style={{ opacity: opacityHero, y: yHero }}
+          src={heroImg}
+          alt="A superhero wearing a cape"
+          id="hero-image"
+        />
       </header>
       <main id="welcome-content">
         <section>
