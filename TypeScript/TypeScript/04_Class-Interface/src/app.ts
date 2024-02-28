ff6c10232fdb1c8e6a3f27c19a60bac764dad4d7
Imshyeon <kzoen0040@naver.com>
2024-02-28 18:13:16 +0900

    📝 클래스와 인터페이스 사용하기

--- TypeScript/TypeScript/04_Class-Interface/src/app.ts
+++ TypeScript/TypeScript/04_Class-Interface/src/app.ts
@@ -1,17 +1,24 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
-user1 = {
-  name: "zoe",
-  age: 23,
+// 두 개 이상의 인터페이스를 사용 가능하다.
+class Person implements Greetable {
+  name: string;
+  age = 23;
+
+  constructor(n: string) {
+    this.name = n;
+  }
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+user1 = new Person("Zoe");
 
 user1.greet("Hi there - I am"); // Hi there - I am zoe
+console.log(user1);
